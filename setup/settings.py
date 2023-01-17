259b7ac233de5cf8175c04f7749380c79c505801
Elberth Junior <elberthasj@hotmail.com>
2023-01-17 11:06:45 -0300

    Django Curso 2 Aula 1

--- setup/settings.py
+++ setup/settings.py
@@ -35,13 +35,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'galeria.apps.GaleriaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'galeria',
 ]
 
 MIDDLEWARE = [
