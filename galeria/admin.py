cfe626fc48c26d2d138eb7341a2acaa3d3a8af8b
Elberth Junior <elberthasj@hotmail.com>
2023-01-18 13:39:13 -0300

    Curso 2 de Django finalizado

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,8 +3,11 @@ from galeria.models import Fotografia
 
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "foto")
+    list_display = ("id", "nome", "foto", "publicada")
     list_display_links = ("id" , "nome")
     search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_editable = ("publicada",)
+    list_per_page = 20
 
 admin.site.register(Fotografia, ListandoFotografias)
